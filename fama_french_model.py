7ffd643654dab241a5ef6ea5b89955d5269c8fa7
KidQuant <38800128+KidQuant@users.noreply.github.com>
2024-11-13 12:41:07 -0500

    Updates

--- fama_french_model.py
+++ fama_french_model.py
@@ -20,42 +20,85 @@ ff_research.head()
 
 df['Date'] = [''.join(x.split('-')[0:2]) for x in df.DATE]
 
-df.interpolate(method='linear', limit_direction='backward', inplace=True)
+# df.interpolate(method='linear', limit_direction='backward', inplace=True)
 
-df = df.merge(ff_research[['Date','macro_smb', 'macro_hml', 'macro_mkt-rf']], on = 'Date', how ='inner' )
+df = df.merge(ff_research[['Date', 'macro_mkt-rf']], on = 'Date', how ='inner' )
+
+df= df[~np.isnan(df['bm'])]
+df=df[~np.isnan(df['mvel1'])]
+
 
 # %%
 from sklearn.model_selection import train_test_split
-
-X = df[['macro_smb', 'macro_hml', 'macro_mkt-rf', 'macro_tbl']]
+X = df[['macro_mkt-rf', 'macro_tbl', 'mvel1', 'bm']]
 y = df[['risk_premium']]
 
-
-
-# %%
-from sklearn.linear_model import LinearRegression
-from sklearn.model_selection import KFold
-
-cv = KFold(n_splits=5, shuffle=True, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 
 # %%
 
-cv_train_score =  []
-cv_test_score = []
-for train_index, test_index in cv.split(X):
-    X_train, X_test = X.iloc[train_index], X.iloc[test_index]
-    y_train, y_test = y.iloc[train_index], y.iloc[test_index]
 
-    lm = LinearRegression()
-    y_pred_lr_train = lm.predict(X_train)
-    y_pred_lr_test = lm.predict(y_test)
+from sklearn.linear_model import LinearRegression, Ridge, Lasso, ElasticNet
+from sklearn.model_selection import KFold, cross_val_score, cross_val_predict
+from sklearn.metrics import r2_score, make_scorer, mean_squared_error 
+
+lm = LinearRegression()
+
+cv = KFold(n_splits=5, shuffle=True, random_state=45)
+r2 = make_scorer(r2_score)
+
+r2_val_score = cross_val_score(lm, X_train, y_train, cv=cv, scoring=r2)
+scores = [r2_val_score.mean()]
+scores
+
     
-    cv_train_score.append(r2_linear_train)
-    cv_test_score.append(cv_test_score)
-    print(cv_train_score.mean())
-    print(cv_test_score.mean())
 
+# %%
+
+from sklearn.cross_decomposition import PLSRegression
+
+def optimise_pls_cv(X, y, n_comp):
+    # Define PLS object
+    pls = PLSRegression(n_components=n_comp)
+
+    # Cross-validation
+    y_cv = cross_val_predict(pls, X, y, cv=10)
+
+    # Calculate scores
+    r2 = r2_score(y, y_cv)
+    mse = mean_squared_error(y, y_cv)
+    rpd = y.std()/np.sqrt(mse)
+    
+    return (y_cv, r2, mse, rpd)
+
+r2s = []
+mses = []
+rpds = []
+xticks = np.arange(1, 5)
+for n_comp in xticks:
+    y_cv, r2, mse, rpd = optimise_pls_cv(X_train, y_train, n_comp)
+    r2s.append(r2)
+    mses.append(mse)
+    rpds.append(rpd)
+
+def plot_metrics(vals, ylabel, objective):
+    with plt.style.context('ggplot'):
+        plt.plot(xticks, np.array(vals), '-v', color='blue', mfc='blue')
+        if objective=='min':
+            idx = np.argmin(vals)
+        else:
+            idx = np.argmax(vals)
+        plt.plot(xticks[idx], np.array(vals)[idx], 'P', ms=10, mfc='red')
+
+        plt.xlabel('Number of PLS components')
+        plt.xticks = xticks
+        plt.ylabel(ylabel)
+        plt.title('PLS')
+
+    plt.show()
     
 
+plot_metrics(mses, 'MSE', 'min')
+plot_metrics(rpds, 'RPD', 'max')
 # %%
