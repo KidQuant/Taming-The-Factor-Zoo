c293eb55db7ecacb2fad229413198d56041a1930
KidQuant <38800128+KidQuant@users.noreply.github.com>
2024-11-22 03:41:18 -0500

    Updates

--- TimeBasedCV.py
+++ TimeBasedCV.py
@@ -1,3 +1,5 @@
+
+#Program that executes the rolling window train-val-test split 
 import pandas as pd
 import datetime
 from datetime import datetime as dt
@@ -20,14 +22,10 @@ class TimeBasedCV(object):
     '''
     
     
-    def __init__(self, 
-                 train_period=30, 
-                 val_period=7, 
-                #  test_period=7, 
-                 freq='days'):
+    def __init__(self, train_period=30, val_period=7, test_period=7, freq='days'):
         self.train_period = train_period
         self.val_period = val_period
-        # self.test_period = test_period
+        self.test_period = test_period
         self.freq = freq
 
         
@@ -63,7 +61,7 @@ class TimeBasedCV(object):
                     
         train_indices_list = []
         val_indices_list = []
-        # test_indices_list = []
+        test_indices_list = []
 
        
         
@@ -71,11 +69,10 @@ class TimeBasedCV(object):
         end_train = start_train + eval('relativedelta('+self.freq+'=self.train_period)')
         start_val = end_train + eval('relativedelta('+self.freq+'=gap)')
         end_val = start_val + eval('relativedelta('+self.freq+'=self.val_period)')
-        # start_test = end_val + eval('relativedelta('+self.freq+'=gap)')
-        # end_test = start_test + eval('relativedelta('+self.freq+'=self.test_period)')
+        start_test = end_val + eval('relativedelta('+self.freq+'=gap)')
+        end_test = start_test + eval('relativedelta('+self.freq+'=self.test_period)')
 
-        # while end_test < data[date_column].max().date():
-        while end_val < data[date_column].max().date():
+        while end_test < data[date_column].max().date():
             # train indices:
             cur_train_indices = list(data[(data[date_column].dt.date>=start_train) & 
                                      (data[date_column].dt.date<end_train)].index)
@@ -84,32 +81,26 @@ class TimeBasedCV(object):
                                      (data[date_column].dt.date<end_val)].index)
 
             # test indices:
-            # cur_test_indices = list(data[(data[date_column].dt.date>=start_test) &
-            #                         (data[date_column].dt.date<end_test)].index)
+            cur_test_indices = list(data[(data[date_column].dt.date>=start_test) &
+                                    (data[date_column].dt.date<end_test)].index)
             
-            print("Train period:",start_train,"-" , end_train, 
-                  ",val period:",start_val,"-" , end_val, 
-                #   ", Test period", start_test, "-", end_test,
-                  "# train records", len(cur_train_indices),
-                  ",# val records", len(cur_val_indices) , 
-                #   ", # test records", len(cur_test_indices)
-                  )
+            print("Train period:",start_train,"-" , end_train, ",val period:",start_val,"-" , end_val, ", Test period", start_test, "-", end_test,
+                  "# train records", len(cur_train_indices),",# val records", len(cur_val_indices) , ", # test records", len(cur_test_indices))
 
             train_indices_list.append(cur_train_indices)
             val_indices_list.append(cur_val_indices)
-            # test_indices_list.append(cur_test_indices)
+            test_indices_list.append(cur_test_indices)
 
             # update dates:
             start_train = start_train + eval('relativedelta('+self.freq+'=self.val_period)')/2
             end_train = start_train + eval('relativedelta('+self.freq+'=self.train_period)')
             start_val = end_train + eval('relativedelta('+self.freq+'=gap)')
             end_val = start_val + eval('relativedelta('+self.freq+'=self.val_period)')            
-            # start_test = end_val + eval('relativedelta('+self.freq+'=gap)')
-            # end_test = start_test + eval('relativedelta('+self.freq+'=self.test_period)')
+            start_test = end_val + eval('relativedelta('+self.freq+'=gap)')
+            end_test = start_test + eval('relativedelta('+self.freq+'=self.test_period)')
 
         # mimic sklearn output  
-        # index_output = [(train,val,test) for train,val,test in zip(train_indices_list,val_indices_list,test_indices_list)]
-        index_output = [(train,val) for train,val in zip(train_indices_list,val_indices_list)]
+        index_output = [(train,val,test) for train,val,test in zip(train_indices_list,val_indices_list,test_indices_list)]
 
         self.n_splits = len(index_output)
         
